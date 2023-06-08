cc3fb54055f55a972a7a99a8075a2a4715a6d49f
Matas951 <matulis321@gmail.com>
2023-06-08 21:08:02 +0300

    Finished email feature

--- email_info.py
+++ email_info.py
@@ -1,16 +1,18 @@
 import ssl, smtplib
+import os
+
 
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
+    context = ssl.create_default_context()
 
     username = "asmatas02@gmail.com"
-    password = "usnellxvefloqoxh"
+    password = os.getenv("PASSWORD")
+    # password = "usnellxvefloqoxh"
 
     receiver = "asmatas02@gmail.com"
-
-    context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -7,15 +7,19 @@ st.header("Contact Us")
 
 
 with st.form(key="email_form"):
-    user_email = st.text_input("Your email address")
-    raw_message = st.text_area("Your message")
+    user_email = st.text_input("Your email address", key="email")
+    topics = st.selectbox("With what do you need help?",
+                          options=["Food", "Cars", "Pictures"])
+    raw_message = st.text_area("Your message", key="message")
     message = f"""\
 Subject: New email from {user_email}
 
 From: {user_email}
+Topic:{topics}
 {raw_message}
 """
     button = st.form_submit_button("Submit")
     if button:
         send_email(message)
         st.info("Your email was sent!")
+
